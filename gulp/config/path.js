8fdb0bed89741094142fe4ca7d67dfa5ae1ca32d
Deddoctor30 <alinsbomb@yandex.ru>
2022-01-20 16:44:14 +0300

    20.01.2022 (2)

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -8,14 +8,17 @@ const srcFolder = `./src`;
 
 export const path = {
    build: {
+      css: `${builFolder}/css/`,
       html: `${builFolder}/`,
       files: `${builFolder}/files/`,
    },
    src: {
+      scss: `${srcFolder}/scss/style.scss`,
       html: `${srcFolder}/*.html`,
       files: `${srcFolder}/files/**/*.*`,
    },
    watch: {
+      scss: `${srcFolder}/scss/**/*.scss`,
       html: `${srcFolder}/**/*.html`,
       files: `${srcFolder}/files/**/*.*`,
    },
