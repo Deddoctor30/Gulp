e5094c233c4d81b2bc8f7830c0d793f605a0f767
Deddoctor30 <alinsbomb@yandex.ru>
2022-01-25 14:02:56 +0300

    25.01.2022

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+// Пример подключения через ES6
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// const swiper = new Swiper();
